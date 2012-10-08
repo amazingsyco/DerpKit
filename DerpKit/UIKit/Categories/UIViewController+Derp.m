c6141cfcba55ef591b76a319d8c61a83175048fc
Yang Meyer <mail@yangmeyer.de>
2012-10-08 14:01:04 +0200

    Add support for customizing minimum height of view (notably of view controllers in a custom container) via an options dictionary.

--- DerpKit/UIKit/Categories/UIViewController+Derp.m
+++ DerpKit/UIKit/Categories/UIViewController+Derp.m
@@ -9,6 +9,31 @@
 #import "UIViewController+Derp.h"
 #import <objc/runtime.h>
 
+const struct DerpKeyboardViewHandlerOptions DerpKeyboardViewHandlerOptions = {
+	.minHeight = @"DerpKeyboardViewHandlerOptionMinimumHeight",
+};
+
+#define kDerpKeyboardViewHandlerOptions @"derp_keyboardViewHandlerOptions"
+#define kDerpKeyboardViewHandlerDefaults @"derp_keyboardViewHandlerDefaults"
+
+@implementation UIViewController (Derp_OptionsAndDefaults)
+
+-(void)derp_registerDefaults {
+	NSDictionary *defaults = @{
+		DerpKeyboardViewHandlerOptions.minHeight : @(0.0),
+	};
+	objc_setAssociatedObject(self, kDerpKeyboardViewHandlerDefaults, defaults, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+-(id)derp_optionsOrDefaultForKey:(NSString*)optionKey {
+	NSDictionary *options = objc_getAssociatedObject(self, kDerpKeyboardViewHandlerOptions);
+	NSDictionary *defaults = objc_getAssociatedObject(self, kDerpKeyboardViewHandlerDefaults);
+	NSAssert(defaults, @"Defaults must have been set when accessing options.");
+	return options[optionKey] ?: defaults[optionKey];
+}
+
+@end
+
 @implementation UIViewController (Derp)
 
 -(BOOL)derp_isViewVisible{
@@ -22,13 +47,20 @@
 }
 
 -(void)derp_addKeyboardViewHandlers{
+	[self derp_addKeyboardViewHandlersWithOptions:nil];
+}
+
+-(void)derp_addKeyboardViewHandlersWithOptions:(NSDictionary*)options{
+	[self derp_registerDefaults];
+	if (options) {
+		objc_setAssociatedObject(self, kDerpKeyboardViewHandlerOptions, options, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	}
 	NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
 	id willShow = [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillShowNotification object:nil queue:mainQueue usingBlock:^(NSNotification *note) {
 		[self derp_performIfVisible:^{
 			CGRect keyboardFrame = [self.view convertRect:[(NSValue *)note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue]
 												 fromView:nil];
-			CGRect viewFrame = self.view.frame;
-			viewFrame.size.height -= keyboardFrame.size.height;
+			CGRect viewFrame = [self derp_viewFrameForKeyboardAppearing:YES toFrame:keyboardFrame];
 			
 			[UIView beginAnimations:@"UIKeyboard" context:nil];
 			
@@ -43,9 +75,9 @@
 	
 	id willHide = [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillHideNotification object:nil queue:mainQueue usingBlock:^(NSNotification *note) {
 		[self derp_performIfVisible:^{
-			CGRect keyboardFrame = [(NSValue *)note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
-			CGRect viewFrame = self.view.frame;
-			viewFrame.size.height += keyboardFrame.size.height;
+			CGRect keyboardFrame = [self.view convertRect:[(NSValue *)note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue]
+												 fromView:nil];
+			CGRect viewFrame = [self derp_viewFrameForKeyboardAppearing:NO toFrame:keyboardFrame];
 			
 			[UIView beginAnimations:@"UIKeyboard" context:nil];
 			
@@ -60,6 +92,22 @@
 	
 	objc_setAssociatedObject(self, "derp_willShowKeyboardNotification", willShow, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 	objc_setAssociatedObject(self, "derp_willHideKeyboardNotification", willHide, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+-(CGRect)derp_viewFrameForKeyboardAppearing:(BOOL)isAppearing toFrame:(CGRect)keyboardFrame{
+	CGFloat minimumHeight = [[self derp_optionsOrDefaultForKey:DerpKeyboardViewHandlerOptions.minHeight] floatValue];
+	CGRect viewFrame = self.view.frame;
+	CGFloat keyboardHeight = (isAppearing ? -1.0 : +1.0) * keyboardFrame.size.height;
+	if (viewFrame.size.height - keyboardFrame.size.height < minimumHeight) {
+		// pin at minimum height and nudge the whole frame up/down
+		viewFrame.size.height = minimumHeight;
+		viewFrame.origin.y = viewFrame.origin.y + keyboardHeight;
+	}
+	else {
+		// just adapt height
+		viewFrame.size.height = viewFrame.size.height + keyboardHeight;
+	}
+	return viewFrame;
 }
 
 -(void)derp_removeKeyboardViewHandlers{
